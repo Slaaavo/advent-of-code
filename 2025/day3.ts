ddce1ded951a60d90736eaa84b2f7988ee60e5dc
Slaaavo <slaaavo@protonmail.com>
2025-12-03 10:36:35 +0100

    add tsconfig

--- 2025/day3.ts
+++ 2025/day3.ts
@@ -5,7 +5,7 @@ function getJoltage(bank: string): number {
     const batteries = bank.split('').map(Number);
     const subBatteries = batteries.slice(0, -1);
     const sortedBatteries = subBatteries.toSorted((a, b) => b - a);
-    const largestBattery = sortedBatteries[0];
+    const largestBattery = sortedBatteries[0] as number
     const largestIndex = batteries.indexOf(largestBattery);
     const batteriesAfterLargest = batteries.slice(largestIndex + 1);
     const sortedBatteriesAfterLargest = batteriesAfterLargest.toSorted((a, b) => b - a);
@@ -28,7 +28,7 @@ function getJoltageGeneric(bank: string, numberOfBatteries: number): number {
 
 function main() {
 
-    const pathToThisFile = process.argv[1]
+    const pathToThisFile = process.argv[1] as string
     const ext = path.extname(pathToThisFile)
     const inputPath = path.join(path.dirname(pathToThisFile), path.basename(pathToThisFile, ext) + ' input.txt')
     const banks = fs.readFileSync(inputPath, 'utf-8').split('\n').filter(line => line.length > 0)
